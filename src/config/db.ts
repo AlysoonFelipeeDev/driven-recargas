b3efb6c6e1ea377959351eebfd4b06d88e946891
jean <alysoonfelipe@hotmail.com>
2025-10-23 18:59:23 -0300

    changes

--- src/config/db.ts
+++ src/config/db.ts
@@ -1,22 +1,20 @@
-import dotenv from "dotenv";
 import { Pool, QueryResult, QueryResultRow } from "pg";
 
-dotenv.config();
+
+if (process.env.NODE_ENV !== "production") {
+    require("dotenv").config();
+}
+
+const isProd = process.env.NODE_ENV === "production";
 
 export const pool = new Pool({
     connectionString: process.env.DATABASE_URL,
-    ssl:
-        process.env.DATABASE_URL?.includes("render.com") ||
-        process.env.DATABASE_URL?.includes("?ssl=true")
-        ? { rejectUnauthorized: false }
-        : undefined,
-    });
+    ssl: isProd ? { rejectUnauthorized: false } : undefined,
+});
 
-    export async function query<T extends QueryResultRow = any>(
+export async function query<T extends QueryResultRow = any>(
     text: string,
     params?: any[]
-    ) {
-    return pool.query<T>(text, params);
+) {
+    return pool.query<T>(text, params) as Promise<QueryResult<T>>;
 }
-
-
